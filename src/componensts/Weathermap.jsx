cf6230cfbf3e94a5e0b1bcaa171062f606887865
Arpit <156578394+arpitbhatia23@users.noreply.github.com>
2024-02-29 12:35:35 +0530

    added

--- src/componensts/Weathermap.jsx
+++ src/componensts/Weathermap.jsx
@@ -1,31 +1,114 @@
 import './weathermap.css'
-import React from 'react'
-import { useEffect } from 'react';
+import React, { useState,useEffect } from 'react'
 import 'leaflet/dist/leaflet.css';
-import {MapContainer,TileLayer} from 'react-leaflet'
-import { useState } from 'react';
+import {MapContainer,TileLayer,LayersControl,Marker,Popup} from 'react-leaflet'
 
 
 export default function Weathermap() {
-    const[position,setposition]=useState()
-const geolocation=()=>{
-    navigator.geolocation.getCurrentPosition((position)=>{
-      const userLocation = [position.coords.latitude, position.coords.longitude];
-console.log(userLocation)
-    
-  setposition(userLocation)
-    })}
-  // }
-  ;useEffect(() => {
-    geolocation();
-  }, [])
 
-    return(
-      <MapContainer center={position} zoom={13}  style={{width:'70vw',height:'41vw'}}>
+const[qeury,setquery]=useState()
+const [position,setPosition]=useState({lat
+  : 
+  31.7128704,
+  lon
+  : 
+  76.0774656})
+const handleGetLocation = async () => {
+  navigator.geolocation.getCurrentPosition(
+    (position) => {
+      if (position && position.coords) {
+     let coords={   lat: position.coords.latitude,
+        lon:position.coords.longitude}
+        setPosition(coords);
+        console.log(coords);
+      } else {
+        console.error("Error: Unable to retrieve position coordinates.");
+      }
+    },
+    (error) => {
+      console.error("Error getting location:", error);
+    }
+  );
+};
+console.log(position)
+
+useEffect(() => {
+ handleGetLocation();
+}, []);
+const handleSearch = () => {
+  search(qeury);
+};  
+// when  we click on enter this fuction run
+const handleKeyPress = (event) => {
+  if (event.key === 'Enter') {
+    search(qeury);
+  }
+};
+
+
+    return(<>
+      <MapContainer center={position} zoom={13}  style={{width:'60vw',height:'41vw',display:'flex',marginTop:'3vw'}}>
         <TileLayer 
-  //  attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
-   url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"/>
-         
+   attribution='<a href="https://www.maptiler.com/copyright/" target="_blank">&copy; MapTiler</a> <a href="https://www.openstreetmap.org/copyright" >&copy; OpenStreetMap contributors</a> <a href="https://openweathermap.org/copyright">&copy; openweather contributors'
+   url={'https://api.maptiler.com/maps/streets-v2/256/{z}/{x}/{y}.png?key=AqYeNWgZfsp2BaEVyM6b'}
+   />
+          <Marker position={position}>
+      <Popup>
+your location
+      </Popup>
+    </Marker>
+    <LayersControl>
+    <LayersControl.Overlay  name='temp'>
+        <TileLayer 
+   attribution='<a href="https://www.maptiler.com/copyright/" target="_blank">&copy; MapTiler</a> <a href="https://www.openstreetmap.org/copyright" >&copy; OpenStreetMap contributors</a> <a href="https://openweathermap.org/copyright">&copy; openweather contributors'
+   url={'https://tile.openweathermap.org/map/temp_new/{z}/{x}/{y}.png?appid=e38b8adced5269e5111dc584c110097a'}
+   />
+    </LayersControl.Overlay>
+    <LayersControl.Overlay  name='pressure'>
+        <TileLayer 
+   attribution='<a href="https://www.maptiler.com/copyright/" target="_blank">&copy; MapTiler</a> <a href="https://www.openstreetmap.org/copyright" >&copy; OpenStreetMap contributors</a> <a href="https://openweathermap.org/copyright">&copy; openweather contributors'
+   url={'https://tile.openweathermap.org/map/pressure_new/{z}/{x}/{y}.png?appid=e38b8adced5269e5111dc584c110097a'}
+   />
+    </LayersControl.Overlay>
+    <LayersControl.Overlay  name='wind'>
+        <TileLayer 
+   attribution='<a href="https://www.maptiler.com/copyright/" target="_blank">&copy; MapTiler</a> <a href="https://www.openstreetmap.org/copyright" >&copy; OpenStreetMap contributors</a> <a href="https://openweathermap.org/copyright">&copy; openweather contributors'
+   url={'https://tile.openweathermap.org/map/wind_new/{z}/{x}/{y}.png?appid=e38b8adced5269e5111dc584c110097a'}
+   />
+    </LayersControl.Overlay>
+    <LayersControl.Overlay  name='clouds'>
+        <TileLayer 
+   attribution='<a href="https://www.maptiler.com/copyright/" target="_blank">&copy; MapTiler</a> <a href="https://www.openstreetmap.org/copyright" >&copy; OpenStreetMap contributors</a> <a href="https://openweathermap.org/copyright">&copy; openweather contributors'
+   url={'https://tile.openweathermap.org/map/clouds_new/{z}/{x}/{y}.png?appid=e38b8adced5269e5111dc584c110097a'}
+   />
+    </LayersControl.Overlay>
+    <LayersControl.Overlay  name='precipitation'>
+        <TileLayer 
+   attribution='<a href="https://www.maptiler.com/copyright/" target="_blank">&copy; MapTiler</a> <a href="https://www.openstreetmap.org/copyright" >&copy; OpenStreetMap contributors</a> <a href="https://openweathermap.org/copyright">&copy; openweather contributors'
+   url={'https://tile.openweathermap.org/map/precipitation_new/{z}/{x}/{y}.png?appid=e38b8adced5269e5111dc584c110097a'}
+   />
+    </LayersControl.Overlay>
+    
+    <LayersControl.Overlay  name='satelite'>
+        <TileLayer 
+   attribution='<a href="https://www.maptiler.com/copyright/" target="_blank">&copy; MapTiler</a> <a href="https://www.openstreetmap.org/copyright" >&copy; OpenStreetMap contributors</a> <a href="https://openweathermap.org/copyright">&copy; openweather contributors'
+   url={'https://api.maptiler.com/maps/satellite/256/{z}/{x}/{y}.jpg?key=AqYeNWgZfsp2BaEVyM6b'}
+   />
+    </LayersControl.Overlay>
+
+
+
+    </LayersControl>
+    
+
       </MapContainer>
+      <div className='search1'>          
+    <input type ="text" onChange={(e)=>setquery(e.target.value)} value={qeury} placeholder="enter location here" onKeyPress={handleKeyPress}/>
+    <button className="bx bx-search" onClick={handleSearch}></button>
+    
+</div>
+     
+      </>
     )
 }
+
